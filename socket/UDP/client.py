9c0598d3f3c3d8a1dc07afa02fdde19b174255b8
LJW0316 <lijingwei010316@163.com>
2021-09-23 21:40:46 +0800

    UDP连接命令行界面

--- socket/UDP/client.py
+++ socket/UDP/client.py
@@ -1,9 +1,47 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = socket.gethostname()
-port = 54321
 
-s.sendto(str.encode("Hello", 'utf-8'), (host, port))
-print(bytes.decode(s.recv(1024)))
-s.close()
+def client():
+    """
+    UDP客户端程序
+    :return:None
+    """
+    # 设置socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = socket.gethostname()
+    port = 54321
+    # 发送验证信息
+    s.sendto(str.encode("Hello", 'utf-8'), (host, port))
+    print(bytes.decode(s.recv(1024)))
+
+    # 输入用户名、密码并选择模式
+    name = input("请输入用户名：")
+    password = input("请输入密码：")
+    print('请选择模式（1登录    2注册）')
+    mode = input()
+
+    # 判断是否登录成功
+    if mode == '1':
+        s.sendto('login'.encode('utf-8'), (host, port))
+    else:
+        s.sendto('submit'.encode('utf-8'), (host, port))
+    # 向server发送相应数据
+    s.sendto(name.encode('utf-8'), (host, port))
+    s.sendto(password.encode('utf-8'), (host, port))
+
+    # 得到返回信息
+    print(s.recv(1024).decode('utf-8'))
+    isConnect = s.recv(10).decode('utf-8')
+
+    # 若连接成功则继续发送消息
+    if isConnect == '1':
+        message = input()
+        while message != 'exit':  # 用户输入exit则退出连接
+            s.sendto(message.encode('utf-8'), (host, port))
+            message = input()
+        s.sendto(message.encode('utf-8'), (host, port))
+    s.close()
+
+
+if __name__ == '__main__':
+    client()
--- socket/UDP/server.py
+++ socket/UDP/server.py
@@ -2,18 +2,65 @@ import socket
 
 
 def server_UDP():
+    """
+    UDP服务端程序
+    :return:
+    """
+    # 设置socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     host = socket.gethostname()
     port = 54321
     s.bind((host, port))
-
     print("Bind UDP on 54321...")
+
+    user = dict()  # 设置用户字典
     while True:
+        # 获得客户端发送的连接信息
         data, addr = s.recvfrom(1024)
-        print("received from: %s:%s." % addr)
-        print(data.decode())
         s.sendto(str.encode('发送成功', 'utf-8'), addr)
-    s.close()
+        print("received from: {address}".format(address=addr))
+        print('{address}: {msg}'.format(address=addr, msg=data.decode()))
+        mode, addr = s.recvfrom(10)  # 模式（login/submit）
+        mode = mode.decode('utf-8')
+
+        # 获取用户信息
+        userName, addr = s.recvfrom(16)
+        userName = userName.decode('utf-8')
+        userPassword, addr = s.recvfrom(16)
+        userPassword = userPassword.decode('utf-8')
+        # 登录判断
+        if mode == 'login':
+            if userName in user:  # 校验用户信息
+                if userPassword != user[userName]:
+                    s.sendto('密码错误！'.encode('utf-8'), addr)
+                    s.sendto('0'.encode('utf-8'), addr)
+                    print('{address} Connection Denied.'.format(address=addr))
+                    # s.close()
+                    continue
+                s.sendto('登录成功！'.encode('utf-8'), addr)
+                s.sendto('1'.encode('utf-8'), addr)
+                print('{userName} Login'.format(userName=userName))
+            else:
+                s.sendto('用户名不存在！'.encode('utf-8'), addr)
+                s.sendto('0'.encode('utf-8'), addr)
+                print('{address} Connection Denied.'.format(address=addr))
+                # s.close()
+                continue
+        else:
+            s.sendto('注册成功！'.encode('utf-8'), addr)
+            s.sendto('1'.encode('utf-8'), addr)
+            print('{userName} Login'.format(userName=userName))
+            user[userName] = userPassword  # 添加到用户字典
+
+        # 获得客户端发送的消息并显示
+        while True:
+            data, addr = s.recvfrom(1024)
+            if not data or data.decode('utf-8') == 'exit':
+                break
+            print('{name}: {msg}'.format(name=userName, msg=data.decode('utf-8')))
+        print('{name} Logout'.format(name=userName))
+
+    s.close()  # 关闭连接
 
 
 if __name__ == '__main__':
