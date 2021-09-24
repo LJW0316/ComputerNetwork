940dd3f12ea5e4a629bfb97716c31524a3902709
LJW0316 <lijingwei010316@163.com>
2021-09-24 08:19:00 +0800

    添加异常处理，解决客户端异常关闭的处理

--- socket/TCP/server.py
+++ socket/TCP/server.py
@@ -13,47 +13,51 @@ def tcpLink(sock, addr, user):
     """
     print('Accept new connection from {address}'.format(address=addr))
     sock.send("Welcome".encode('utf-8'))
-    mode = sock.recv(10).decode('utf-8')  # 模式（login/submit）
-    # 获取用户信息
-    userName = sock.recv(16).decode('utf-8')
-    userPassword = sock.recv(16).decode('utf-8')
-    if mode == 'login':
-        if userName in user:  # 校验用户信息
-            if userPassword != user[userName]:
-                sock.send('密码错误！'.encode('utf-8'))
+    try:
+        mode = sock.recv(10).decode('utf-8')  # 模式（login/submit）
+        # 获取用户信息
+        userName = sock.recv(16).decode('utf-8')
+        userPassword = sock.recv(16).decode('utf-8')
+        if mode == 'login':
+            if userName in user:  # 校验用户信息
+                if userPassword != user[userName]:
+                    sock.send('密码错误！'.encode('utf-8'))
+                    sock.send('0'.encode('utf-8'))
+                    sock.close()  # 关闭连接
+                    print('{address} Connection Denied.'.format(address=addr))
+                    print('Connection from addr {address} closed.'.format(address=addr))
+                    return
+                sock.send('登录成功！'.encode('utf-8'))
+                sock.send('1'.encode('utf-8'))
+                print('{userName} Login'.format(userName=userName))
+            else:
+                sock.send('用户名不存在！'.encode('utf-8'))
                 sock.send('0'.encode('utf-8'))
                 sock.close()  # 关闭连接
                 print('{address} Connection Denied.'.format(address=addr))
                 print('Connection from addr {address} closed.'.format(address=addr))
                 return
-            sock.send('登录成功！'.encode('utf-8'))
+        else:
+            sock.send('注册成功！'.encode('utf-8'))
             sock.send('1'.encode('utf-8'))
             print('{userName} Login'.format(userName=userName))
-        else:
-            sock.send('用户名不存在！'.encode('utf-8'))
-            sock.send('0'.encode('utf-8'))
-            sock.close()  # 关闭连接
-            print('{address} Connection Denied.'.format(address=addr))
-            print('Connection from addr {address} closed.'.format(address=addr))
-            return
-    else:
-        sock.send('注册成功！'.encode('utf-8'))
-        sock.send('1'.encode('utf-8'))
-        print('{userName} Login'.format(userName=userName))
-        user[userName] = userPassword  # 添加到用户字典
-    # print('userName: {name}'.format(name=userName))
-    # print('userPassword: {password}'.format(password=userPassword))
+            user[userName] = userPassword  # 添加到用户字典
+        # print('userName: {name}'.format(name=userName))
+        # print('userPassword: {password}'.format(password=userPassword))
 
-    # 接收用户消息
-    while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        if not data or data.decode('utf-8') == 'exit':
-            break
-        print('{name}: {msg}'.format(name=userName, msg=data.decode('utf-8')))
+        # 接收用户消息
+        while True:
+            data = sock.recv(1024)
+            time.sleep(1)
+            if not data or data.decode('utf-8') == 'exit':
+                break
+            print('{name}: {msg}'.format(name=userName, msg=data.decode('utf-8')))
 
-    sock.close()  # 关闭连接
-    print('Connection from addr {address} closed.'.format(address=addr))
+        sock.close()  # 关闭连接
+        print('Connection from addr {address} closed.'.format(address=addr))
+    except ConnectionResetError:
+        sock.close()  # 关闭连接
+        print('Connection from addr {address} closed.'.format(address=addr))
 
 
 def server_TCP():
